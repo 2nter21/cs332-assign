878fde917777fe763894aec0bfd919b6a7d0f36e
kminseong841 <155376343+2nter21@users.noreply.github.com>
2024-09-07 15:01:18 +0900

    Update Main.scala
    solved problems in recfun.Main

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,15 +14,34 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0 || r == 0) 1
+    else if (c == r) 1
+    else pascal(c-1, r-1) + pascal(c, r-1)
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def balance_in(chars: List[Char], openCnt: Int): Boolean = 
+    {
+      if(openCnt < 0) false
+      else if(chars.isEmpty && openCnt == 0) true
+      else if(chars.isEmpty && openCnt != 0) false
+      else if(chars.head == '(') balance_in(chars.tail, openCnt + 1)
+      else if(chars.head == ')') balance_in(chars.tail, openCnt - 1)
+      else balance_in(chars.tail, openCnt)
+    }
+    balance_in(chars, 0)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 0) 1
+    else if (money < 0 || coins.isEmpty) 0
+    else countChange(money, coins.tail) + countChange(money - coins.head, coins)
+  }
 }
